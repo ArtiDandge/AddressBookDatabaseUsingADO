f13da1d2a3e36411bac8357a534082c6150c126a
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-29 23:38:57 +0530

    [Arti Dandge] Add : Ability to Insert Contact from application to DB

--- AddressBookDatabaseWithADO/AddressBookRepo.cs
+++ AddressBookDatabaseWithADO/AddressBookRepo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Text;
 
@@ -10,19 +11,37 @@ namespace AddressBookDatabaseWithADO
         public static string connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = addressbook_service; Integrated Security = True";
         SqlConnection connection = new SqlConnection(connectionString);
 
-        public void CheckConnection()
+        public bool AddContact(AddressBookModel model)
         {
             try
             {
                 using (this.connection)
                 {
-                    connection.Open();
-                    Console.WriteLine("Database Connection OK");
+                    SqlCommand command = new SqlCommand("SpAddContactInAddressBook", this.connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@first_name", model.first_name);
+                    command.Parameters.AddWithValue("@last_name", model.last_name);
+                    command.Parameters.AddWithValue("@address", model.address);
+                    command.Parameters.AddWithValue("@city", model.city);
+                    command.Parameters.AddWithValue("@state", model.state);
+                    command.Parameters.AddWithValue("@zip", model.zip);
+                    command.Parameters.AddWithValue("@phone_number", model.phone_number);
+                    command.Parameters.AddWithValue("@email", model.email);
+                    command.Parameters.AddWithValue("@addressbook_name", model.addressbook_name);
+                    command.Parameters.AddWithValue("@addressbook_type", model.addressbook_type);
+                    this.connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
                 }
             }
-            catch
+            catch (Exception e)
             {
-                Console.WriteLine("Database not connected");
+                throw new Exception(e.Message);
             }
             finally
             {
--- AddressBookDatabaseWithADO/Program.cs
+++ AddressBookDatabaseWithADO/Program.cs
@@ -8,7 +8,21 @@ namespace AddressBookDatabaseWithADO
         {
             Console.WriteLine("Welcome to Address Book Database Project with ADO.NET");
             AddressBookRepo repo = new AddressBookRepo();
-            repo.CheckConnection();
+            //repo.CheckConnection();
+            AddressBookModel addressBookModel = new AddressBookModel();
+
+            addressBookModel.first_name = "Mamta";
+            addressBookModel.last_name = "Chaudhary";
+            addressBookModel.address = "Rajmata chauk";
+            addressBookModel.city = "Jaipur";
+            addressBookModel.state = "Rajasthan";
+            addressBookModel.zip = 423233;
+            addressBookModel.phone_number = "7654321095";
+            addressBookModel.email = "mamta1234@gmail.com";
+            addressBookModel.addressbook_name = "AddressBook2";
+            addressBookModel.addressbook_type = "Friends";
+
+            repo.AddContact(addressBookModel);
         }
     }
 }
